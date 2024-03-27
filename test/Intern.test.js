455eb867ea6fc18b0d5d5514094f1887bd038844
bluNite <ncrowe175@gmail.com>
2024-03-27 12:53:52 -0400

    added schooltest intern object test passed blunite

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -4,3 +4,10 @@ test("should represent an instance of Intern class object", () => {
 	const e = new Intern();
 	expect(typeof (e)).toBe("object");
 });
+
+// test for Intern constructor set school to object --checked passed
+test("should return Intern 'school' argument using constructor", () => {
+	const testVal = "ColumbiaUCB";
+	const e = new Intern("Fizz", 50, "internEmail.com", testVal);
+	expect(e.school).toBe(testVal);
+})
