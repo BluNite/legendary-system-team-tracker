624cabea7a420c42fb7819e9f319db03d4385c88
bluNite <ncrowe175@gmail.com>
2024-03-27 13:17:53 -0400

    added getRole() test passed blunite

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -10,4 +10,12 @@ test("should return Intern 'school' argument using constructor", () => {
 	const testVal = "ColumbiaUCB";
 	const e = new Intern("Fizz", 50, "internEmail.com", testVal);
 	expect(e.school).toBe(testVal);
-})
+});
+
+// test for getRole() should return 'Intern' --checked passed
+test("getRole() should return " / Intern / "", () => {
+	const testVal = "Intern";
+	const e = new Intern("Fizz", 50, "internEmail.com", "ColumbiaUCB");
+	expect(e.getRole()) === testVal;
+});
+
