53148a379b393cee405f604f1d3125e08eddd327
bluNite <ncrowe175@gmail.com>
2024-03-26 23:19:04 -0400

    running test blumite

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -5,3 +5,12 @@ test("Should represent an instance of class Employee", () => {
 	const e = new Employee();
 	expect(typeof (e)).toBe("object");
 });
+
+test("Should set name using constructor argument", () => {
+
+	const e = new Employee(name);
+	const name = "Bob";
+	expect(e.name).toBe(name);
+
+});
+
