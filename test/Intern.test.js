100b4bd095deb78c88062b2eea4c56798981e3c0
bluNite <ncrowe175@gmail.com>
2024-03-27 12:32:11 -0400

    note added on Intern object test passed pushing to repo blunite

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,5 +1,5 @@
 const Intern = require('../lib/Intern');
-// Intern object test 
+// Intern object test --checked passed
 test("should represent an instance of Intern class object", () => {
 	const e = new Intern();
 	expect(typeof (e)).toBe("object");
