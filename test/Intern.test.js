d260aa38fc04a2cc24da25d84024d90774dbdef3
bluNite <ncrowe175@gmail.com>
2024-03-27 12:31:04 -0400

    correction to object test and ran test passed blunite

--- test/Intern.test.js
+++ test/Intern.test.js
@@ -1,1 +1,6 @@
 const Intern = require('../lib/Intern');
+// Intern object test 
+test("should represent an instance of Intern class object", () => {
+	const e = new Intern();
+	expect(typeof (e)).toBe("object");
+});
