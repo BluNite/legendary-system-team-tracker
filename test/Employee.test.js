634d9a59b25a9aac85e44900a71e9df9919c0d97
bluNite <ncrowe175@gmail.com>
2024-03-27 09:42:26 -0400

    gi

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -6,18 +6,27 @@ test("Should represent an instance of class Employee", () => {
 	expect(typeof (e)).toBe("object");
 });
 
-// test to set name constructor argument -- check passed
+// test to set constructor argument 'name'  -- check passed
 test("Should set name using constructor argument", () => {
 	const name = "Bob";
 	const e = new Employee(name);
-	expect(e.name).toBe(name);
+	expect(e.name) === (name);
 
 });
 
-test("Should set id using constructor argument", () => {
+// test to set constructor argument 'id' 
+test("Should set id  using constructor argument 'id' ", () => {
 	const testVal = 50;
 	const e = new Employee("Fizz", testVal);
-	expect(e.id).toBe(testVal);
+	expect(e.id) === (testVal);
 
 });
 
+// test to set constructor argument 'email'
+
+test("Should set email constructor argument 'email' ", () => {
+	const testVal = "MacTheFry@fakeMail.com";
+	const e = new Employee("Bob", 4, testVal);
+	expect(e.email) === (testVal);
+
+});
